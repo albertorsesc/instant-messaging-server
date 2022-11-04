f49602bb117fbf9a2a5a57778ba484550834d77c
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-11-03 21:58:22 -0700

    Apollo GraphQL Server + User schema definitions

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,36 @@
-// npm install @apollo/server express graphql cors body-parser
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import express from 'express';
 import http from 'http';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import { typeDefs, resolvers } from './schema';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
-interface MyContext {
-  token?: String;
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers/index.resolver';
+
+async function main() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  });
+
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/',
+  });
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
-// Required logic for integrating with Express
-const app = express();
-// Our httpServer handles incoming requests to our Express app.
-// Below, we tell Apollo Server to "drain" this httpServer,
-// enabling our servers to shut down gracefully.
-const httpServer = http.createServer(app);
-
-// Same ApolloServer initialization as before, plus the drain plugin
-// for our httpServer.
-const server = new ApolloServer<MyContext>({
-  typeDefs,
-  resolvers,
-  plugins: [
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-  ],
-});
-// Ensure we wait for our server to start
-await server.start();
-
-// Set up our Express middleware to handle CORS, body parsing,
-// and our expressMiddleware function.
-app.use(
-  '/',
-  cors<cors.CorsRequest>(),
-  bodyParser.json(),
-  // expressMiddleware accepts the same arguments:
-  // an Apollo Server instance and optional configuration options
-  expressMiddleware(server, {
-    context: async ({ req }) => ({ token: req.headers.token }),
-  }),
-);
-
-// Modified server startup
-await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000/`);
+main().catch(error => console.log(error));
